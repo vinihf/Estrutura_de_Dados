d30874923b2f3bd2a5cee8f21d711816efab3f23
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2022-11-28 11:08:52 -0300

    Arvores

---
+++ Árvore/arvoreBB.py
@@ -0,0 +1,30 @@
+from structlinks.DataStructures import BinarySearchTree
+
+# Criando uma árvore a partir de uma lista
+bst = BinarySearchTree.create_tree([1958,1962,1970,1994])
+
+# Imprimindo a árvore
+print(bst)
+
+# Inserindo item na árvore
+bst.insert(2002)
+
+# Imprimindo a árvore
+bst.display()
+
+# Raiz da árvore
+print(bst.root)
+
+# Altura da árvore
+print(bst.height)
+
+# É balanceada?
+print(bst.is_balanced)
+
+# Contém item?
+print(2022  in bst)
+
+# Remove item
+bst.remove(1994)
+
+bst.display()
