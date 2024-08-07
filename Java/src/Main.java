c9a08d364ea2b0ef32694754abc1d19219898133
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2024-08-07 19:06:49 -0300

    Simples

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void SelectionSort(int[] array) {
